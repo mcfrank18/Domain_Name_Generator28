6d3845b7e8e8f9fbe881f738589daf6f06abc661
mcfrank18 <fmperezg2010@gmail.com>
2022-07-21 00:55:06 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let p = 0; p < 2; p++) {
+    for (let a = 0; a < 2; a++) {
+      for (let n = 0; n < 2; n++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+      }
+    }
+  }
 };
